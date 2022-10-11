cb574c878d2aa14ce048f2bb8b17d7273d51d125
usmc1371nerd <james.p.dumas@gmail.com>
2022-10-10 21:49:34 -0500

    debugger

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -56,7 +56,14 @@ export default class PortfolioContainer extends Component{
     portfolioItems(){
         
         return this.state.data.map(item => {
-            return <PortfolioItem title={item.name} url={item.url} slug={item.id}/>; 
+            debugger;
+            return ( <PortfolioItem 
+            key={item.id} 
+            title={item.id}
+            url={item.url} 
+            slug={item.id}
+            />
+            );
         });
     }
  
